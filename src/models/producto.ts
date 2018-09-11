9b28feb00bc0c06d4aee58add4faa493aff5c121
liliana <andresroldanma@gmail.com>
2018-09-11 11:13:59 -0500

    tienda-66

---
+++ src/models/producto.ts
@@ -0,0 +1,10 @@
+export class Producto
+{
+    id?: string;
+    bestSeller?: boolean;
+    nombre?: string;
+    descripcion?: string;
+    precio?: string;
+    imagen?: string;
+    genero?: string
+}
